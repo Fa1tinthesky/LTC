00be9d719819da819d1b79c3ac37712a8af8efd6
Muhammad <kayumovmuhamma@gmail.com>
2025-12-06 14:57:07 +0500

    add: readme

---
+++ README.md
@@ -0,0 +1,5 @@
+- LTC
+
+```
+  uvicorn app.main:app --reload
+```
--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+
 from api.routes import reports
 
 app = FastAPI()
 
 app.include_router(reports.router, prefix="", tags=["reports"])
+
 
 @app.get("/")
 async def root():
